e1c19e482f013c2716f12312390e17cefab1e251
nanaplouf <vera78ds@gmail.com>
2025-09-08 10:16:13 +0200

    cours get

--- partials/head.php
+++ partials/head.php
@@ -27,6 +27,7 @@
                 <a class="nav-link" href="08-fonction.php">fonction</a>
                 <a class="nav-link" href="09-date.php">date</a>
                 <a class="nav-link" href="10-inclusion.php">fonction</a>
+                <a class="nav-link" href="11-requetes.php">requêtes</a>
             </div>
             </div>
         </div>
